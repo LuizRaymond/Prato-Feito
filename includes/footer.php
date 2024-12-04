9fb6b79efecc5e0d82ab787d20890764fc26614d
LuizRaymond <luizinholegal13@gmail.com>
2024-12-03 23:04:34 -0300

    feat: add footer component

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<footer class="p-4 text-white flex items-center justify-around" style="background-color: #162415;">
+    <p>&copy; 2024 Prato Feito</p>
+    <div>
+        <h2>Colaboradores:</h2>
+        <ul>
+            <li class="text-sm">Luiz Raimundo</li>
+            <li class="text-sm">Kauan Hikaru</li>
+            <li class="text-sm">Lucas Gomes</li>
+            <li class="text-sm">Renan Enzo</li>
+        </ul>
+    </div>
+</footer>
+</body>
+</html>
